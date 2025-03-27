94664401da0560e969ff871dd51c47a6a888baab
DAMIE Marc <marc.damie@inria.fr>
2025-03-27 19:40:37 +0100

    Init the package

---
+++ fediverse_graphs/main.py
@@ -0,0 +1,4 @@
+import mlcroissant as mlc
+import networkx as nx
+
+CROISSANT_URL = "https://www.kaggle.com/datasets/marcdamie/fediverse-graph-dataset/croissant/download"
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fediverse-graphs",
+    version="0.0.1",
+    author="Marc DAMIE",
+    author_email="marc.damie@inria.fr",
+    description="Interface to download and interact with the Fediverse Graph Dataset",
+    packages=find_packages(),
+    license="GPLv3",
+    python_requires=">=3.8",
+    install_requires=[
+        "mlcroissant",
+        "networkx",
+    ],
+)
