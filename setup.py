2bb9b60a7f333796c8c3ef38674bd03cb585c267
DAMIE Marc <marc.damie@inria.fr>
2025-03-29 19:33:32 +0100

    Bump package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="fediverse-graphs",
-    version="0.0.2",
+    version="0.9.0",
     author="Marc DAMIE",
     author_email="marc.damie@inria.fr",
     description="Interface to download and interact with the Fediverse Graph Dataset",
